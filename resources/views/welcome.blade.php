f63625788b401cce205827a952bd3e60e4a06a52
anonymoussc <50c5ac69@opayq.com>
2015-08-19 21:49:58 +0700

    Determining Variable Existence

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
 <body>
     <div class="container">
         <div class="content">
-            <div class="title">Welcome</div>
+            <div class="title">Welcome, {{ $planeta or 'Ausus' }}</div>
             Nunquam reperire sensorem {{ $luras }} on {{ $date }}.
         </div>
     </div>
